36ae7aa7a82e2825da75386c11c123b13b321f98
krazyunderground <charlieprescod@outlook.com>
2022-08-31 11:28:43 +0100

    deleted:    sandbox.config.json 	modified:   src/env.d.ts 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+// Use this file to type your environment variables!
+// See https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript for more information
+
+interface ImportMetaEnv {
+	readonly PUBLIC_AUTH_TOKEN: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
